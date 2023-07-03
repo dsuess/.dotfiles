88a4d3faf3cc048c49ad35f3e2f74a4fff52f038
Daniel Suess <suess.daniel@gmail.com>
2023-07-03 21:13:37 +1000

    Add homebrew setup

---
+++ homebrew-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+brew tap homebrew/cask-versions
+
+for TGT in "karabiner-elements bettertouchtool 1password iterm2 alfred visual-studio-code-insiders google-chrome"
+  do
+  brew install --cask $TGT
+  done
+
+brew update
+brew install git zsh neovim fzf thefuck just php htop gnupg direnv tmux
+
+
