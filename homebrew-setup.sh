67b4eb2a6cfd5adff2c25f379bbe8492905d32cf
Daniel Suess <suess.daniel@gmail.com>
2023-08-08 11:27:48 +1000

    Update setup script

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -2,12 +2,12 @@
 
 brew tap homebrew/cask-versions
 
-for TGT in "karabiner-elements bettertouchtool 1password iterm2 alfred visual-studio-code-insiders google-chrome"
+for TGT in "karabiner-elements bettertouchtool 1password iterm2 alfred visual-studio-code-insiders google-chrome dash"
   do
   brew install --cask $TGT
   done
 
 brew update
-brew install git zsh neovim fzf thefuck just php htop gnupg direnv tmux
+brew install git zsh neovim fzf thefuck just php htop gnupg direnv tmux openssl pyenv
 
 
